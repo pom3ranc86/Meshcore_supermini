fe0996de98894bd947b116ea49a61a68289a3c17
Pom3ranc86 <david@frel.cz>
2026-06-07 11:01:54 +0200

    Pridana vlastni varianta SuperMini: nRF52840 SuperMini + HT-RA62, bez displeje

---
+++ variants/supermini/target.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+#include "target.h"
+#include <helpers/ArduinoHelpers.h>
+
+PromicroBoard board;
+RADIO_CLASS radio = new Module(P_LORA_NSS, P_LORA_DIO_1, P_LORA_RESET, P_LORA_BUSY, SPI);
+WRAPPER_CLASS radio_driver(radio, board);
+VolatileRTCClock fallback_clock;
+AutoDiscoverRTCClock rtc_clock(fallback_clock);
+EnvironmentSensorManager sensors;
+NullDisplayDriver display;
+
+bool radio_init() {
+  rtc_clock.begin(Wire);
+  return radio.std_init(&SPI);
+}
+
+mesh::LocalIdentity radio_new_identity() {
+  RadioNoiseListener rng(radio);
+  return mesh::LocalIdentity(&rng);
+}
---
+++ variants/supermini/target.h
@@ -0,0 +1,18 @@
+#pragma once
+#define RADIOLIB_STATIC_ONLY 1
+#include <RadioLib.h>
+#include <helpers/radiolib/RadioLibWrappers.h>
+#include <PromicroBoard.h>
+#include <helpers/radiolib/CustomSX1262Wrapper.h>
+#include <helpers/AutoDiscoverRTCClock.h>
+#include <helpers/ui/NullDisplayDriver.h>
+#include <helpers/sensors/EnvironmentSensorManager.h>
+
+extern PromicroBoard board;
+extern WRAPPER_CLASS radio_driver;
+extern AutoDiscoverRTCClock rtc_clock;
+extern EnvironmentSensorManager sensors;
+extern NullDisplayDriver display;
+
+bool radio_init();
+mesh::LocalIdentity radio_new_identity();
